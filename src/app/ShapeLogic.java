5f0b35d7481b924f9275665ea2de92156679ef29
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-05-20 22:26:29 +0200

    Add controlLoop() extends in ShapeLogic

--- src/app/ShapeLogic.java
+++ src/app/ShapeLogic.java
@@ -3,6 +3,8 @@ package app;
 import io.ConsolePrinter;
 import io.DataReader;
 
+import java.util.InputMismatchException;
+
 public class ShapeLogic {
     ConsolePrinter printer = new ConsolePrinter();
     DataReader dataReader = new DataReader(printer);
@@ -12,10 +14,41 @@ public class ShapeLogic {
     }
 
     private void controlLoop(){
-        getOptions();
+        Options options;
+
+        do{
+            options = getOptions();
+            switch (options){
+                case CIRCLE:
+                    dataReader.calculateCircle();
+                    break;
+                case TRIANGLE:
+                    dataReader.calculateTriangle();
+                    break;
+                case RECTANGLE:
+                    dataReader.calculateRectangle();
+                    break;
+
+            }
+        }while (options != Options.EXIT);
     }
 
-    private void getOptions(){
+    private Options getOptions(){
+        boolean isOptionsOk = false;
+        Options options = null;
+        while (!isOptionsOk){
+            printOptions();
+            try {
+                options = Options.createFromInt(dataReader.getInt());
+                isOptionsOk = true;
+            }catch(InputMismatchException ex){
+                printer.printLine("We don't have that options. Try again");
+            }
+        }
+        return options;
+    }
+
+    private void printOptions(){
         printer.printLine("Choose options:");
         for(Options options : Options.values()){
             printer.printLine(options.toString());
